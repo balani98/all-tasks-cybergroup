34815cdb05656d7d8433fee7bb6fa643fe8fc47f
balani98 <balanideepanshu92@gmail.com>
2020-01-16 11:31:01 +0530

    all the tasks of javascript are added

---
+++ javascript/practice.js
@@ -0,0 +1,17 @@
+var a=3;
+var b=true;
+c={key:'value',test:'testing'};
+function hello(){
+   console.log("A sample log");
+   document.getElementById("id").innerHTML="demo";
+     var a=2;
+     alert("i am inside hello"+a);
+     myfunc(a);
+     console.log(typeof(b));
+     console.log(typeof(null));
+    console.log(c.key);
+
+
+function myfunc(){
+ alert("i am inside myfunc"+a);
+}
